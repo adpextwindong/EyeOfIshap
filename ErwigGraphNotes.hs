ab1c94cd1fec3738a08819ae32058d49c996ef1b
Takumi Crary <tcrary@gmail.com>
2021-11-02 09:02:29 +0100

    Add graph notes

---
+++ ErwigGraphNotes.hs
@@ -0,0 +1,6 @@
+{-
+https://hackage.haskell.org/package/fgl
+http://web.engr.oregonstate.edu/~erwig/fgl/haskell/
+http://web.engr.oregonstate.edu/~erwig/papers/InductiveGraphs_JFP01.pdf
+https://web.engr.oregonstate.edu/~erwig/fpg/
+-}
