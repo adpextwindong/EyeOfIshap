a1d402c80ab0af7bb44086b262281c98856b3ba3
Takumi Crary <tcrary@gmail.com>
2021-11-04 08:59:00 +0100

    Add bit from FGL paper

--- ErwigGraphNotes.hs
+++ ErwigGraphNotes.hs
@@ -4,3 +4,15 @@ http://web.engr.oregonstate.edu/~erwig/fgl/haskell/
 http://web.engr.oregonstate.edu/~erwig/papers/InductiveGraphs_JFP01.pdf
 https://web.engr.oregonstate.edu/~erwig/fpg/
 -}
+
+--"More or less equiv according to codingbuddy"
+
+class (Eq a) => Node a where
+    gen :: Int -> [a]
+    new :: [a] -> a
+
+data GNode = GNode
+    deriving Eq
+
+instance Node GNode
+--TODO
