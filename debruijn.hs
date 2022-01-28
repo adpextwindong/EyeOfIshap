d76b467ad0af9814c145b61e8cb5fd70e6bc7138
Takumi Crary <tcrary@gmail.com>
2022-01-28 02:35:38 +0000

    Add some prettyPrinting for debruijn indices

--- debruijn.hs
+++ debruijn.hs
@@ -6,13 +6,24 @@ data Term v
   | App (Term v) (Term v)
   deriving Show
 
-prettyDebruijn :: Term Char -> Term Int
-prettyDebruijn = toDebruijn M.empty
+toDebruijn :: Term Char -> Term Int
+toDebruijn = toDebruijn' M.empty
 
-toDebruijn :: M.Map Char Int -> Term Char -> Term Int
-toDebruijn m (Var v) = Var (m M.! v)
-toDebruijn m (App l r) = App (toDebruijn m l) (toDebruijn m r)
-toDebruijn m (Lam b t) = Lam 0 (toDebruijn (M.insert b 0 (incrM m)) t)
+prettyPrint :: Term Char -> IO ()
+prettyPrint = putStrLn . pretty
+
+pretty :: Term Char -> String
+pretty = pretty' . toDebruijn
+
+pretty' :: Term Int -> String
+pretty' (Var i)   = show i
+pretty' (Lam _ t) = "λ." <> pretty' t
+pretty' (App l r) = "(" <> pretty' l <> " " <> pretty' r <> ")"
+
+toDebruijn' :: M.Map Char Int -> Term Char -> Term Int
+toDebruijn' m (Var v) = Var (m M.! v)
+toDebruijn' m (App l r) = App (toDebruijn' m l) (toDebruijn' m r)
+toDebruijn' m (Lam b t) = Lam 0 (toDebruijn' (M.insert b 0 (incrM m)) t)
   where
     incrM = fmap (+ 1) :: M.Map Char Int -> M.Map Char Int
 
@@ -21,3 +32,5 @@ tx = Lam 'x' (Lam 'y' (Var 'x'))
 
 ty :: Term Char
 ty = Lam 'x' (Lam 'y' (Lam 'z' (App (App (Var 'x') (Var 'y')) (Var 'z'))))
+
+tz = App ty ty
